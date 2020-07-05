621915722aa2e9336f46b9ea08c6ec2216fd1d60
jacob-oreilly <jakeor45@gmail.com>
2020-07-04 21:40:11 -0500

    Iso metric grid system for laying out arena

--- main.lua
+++ main.lua
@@ -1,3 +1,35 @@
+function love.load()
+    love.window.setMode(1280, 800, {resizable=true, vsync=false, minwidth= 400, minheight=300})
+    grid_x = 600
+    grid_y = 400
+    floor = {}
+    tile = love.graphics.newImage('floor.png')
+    block_width = tile:getWidth()
+    block_height = tile:getHeight()
+    block_depth = block_height / 2
+    grid_size = 15
+    grid = {}
+    for x = 1, grid_size do 
+        grid[x] = {}
+        for y = 1, grid_size do
+            grid[x][y] = 1
+        end
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    red = 88 / 250
+    green = 202 / 250
+    blue = 202 / 250
+    color = {red, green, blue}
+    love.graphics.setBackgroundColor(color)
+
+    for x = 1, grid_size do
+        for y = 1, grid_size do
+            love.graphics.draw(tile,
+                grid_x + ((y-x) * (block_width /2)),
+                grid_y + ((x+y) * (block_depth / 2)) - (block_depth * (grid_size / 2)) - block_depth)
+        end
+    end
+    
 end
